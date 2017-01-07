11aaf4fa9202bd4dfe30ba779503bc44b48e3fb8
FinalDoom <FinalDoom@gmail.com>
2017-01-06 20:26:03 -0500

    Reverted test

--- app/src/test/java/com/hiddenramblings/tagmo/ExampleUnitTest.java
+++ app/src/test/java/com/hiddenramblings/tagmo/ExampleUnitTest.java
@@ -9,18 +9,9 @@ import static org.junit.Assert.*;
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
-public class ExampleUnitTest  {
+public class ExampleUnitTest {
     @Test
-    public static void main(String... args) throws Exception {
+    public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
-
-        int brand = 0x08 & 0xFF << 8;
-        brand |= (0x01 & 0xFF);
-
-        int variant = 0x00 & 0xFF;
-
-        int type = 0x00 & 0xFF;
-
-        assertEquals(0x08010000, brand << 12 & variant << 8 & type );
     }
 }

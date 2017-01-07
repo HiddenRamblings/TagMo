07befe2a88c707af8aaa7bb6c85cb219e0b0e999
FinalDoom <FinalDoom@gmail.com>
2017-01-06 20:09:48 -0500

    Fixed fallback naming and series grabbing, upped version

--- app/src/test/java/com/hiddenramblings/tagmo/ExampleUnitTest.java
+++ app/src/test/java/com/hiddenramblings/tagmo/ExampleUnitTest.java
@@ -1,5 +1,7 @@
 package com.hiddenramblings.tagmo;
 
+import android.test.ActivityTestCase;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,9 +9,18 @@ import static org.junit.Assert.*;
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
-public class ExampleUnitTest {
+public class ExampleUnitTest  {
     @Test
-    public void addition_isCorrect() throws Exception {
+    public static void main(String... args) throws Exception {
         assertEquals(4, 2 + 2);
+
+        int brand = 0x08 & 0xFF << 8;
+        brand |= (0x01 & 0xFF);
+
+        int variant = 0x00 & 0xFF;
+
+        int type = 0x00 & 0xFF;
+
+        assertEquals(0x08010000, brand << 12 & variant << 8 & type );
     }
 }

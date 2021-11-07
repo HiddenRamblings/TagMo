3261ee1c5f874438f97cd15a77f4fd149109eb9d
Abandoned Cart <twistedumbrella@gmail.com>
2021-11-07 14:41:25 -0500

    Handle variance in key file naming conventions -/_
    Thanks to @yeeshue99 for identifying the issue with some Samsung Galaxy devices

--- app/src/main/java/com/hiddenramblings/tagmo/nfctech/KeyManager.java
+++ app/src/main/java/com/hiddenramblings/tagmo/nfctech/KeyManager.java
@@ -3,14 +3,17 @@ package com.hiddenramblings.tagmo.nfctech;
 import android.content.Context;
 import android.net.Uri;
 
+import com.eightbit.content.DocumentsUri;
 import com.eightbit.io.Debug;
 import com.hiddenramblings.tagmo.R;
 import com.hiddenramblings.tagmo.TagMo;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Locale;
 
 public class KeyManager {
 
@@ -97,6 +100,21 @@ public class KeyManager {
     }
 
     public void loadKey(Uri file) throws IOException {
+        String keyPath = DocumentsUri.getPath(TagMo.getContext(), file);
+        if (keyPath == null)
+            throw new IOException(TagMo.getStringRes(R.string.key_read_error));
+        File keyFile = new File(keyPath);
+        String fileName = keyFile.getName().toLowerCase(Locale.ROOT);
+        if (!fileName.equals(FIXED_KEY_FILE) && !fileName.equals(UNFIXED_KEY_FILE)) {
+            if (fileName.startsWith("unfixed") && fileName.endsWith("key.bin")) {
+                //noinspection ResultOfMethodCallIgnored
+                keyFile.renameTo(new File(keyFile.getParent(), UNFIXED_KEY_FILE));
+            }
+            if (fileName.startsWith("fixed") && fileName.endsWith("key.bin")) {
+                //noinspection ResultOfMethodCallIgnored
+                keyFile.renameTo(new File(keyFile.getParent(), FIXED_KEY_FILE));
+            }
+        }
         try (InputStream strm = context.getContentResolver().openInputStream(file)) {
             if (!readKey(strm))
                 throw new IOException(TagMo.getStringRes(R.string.invalid_key_error));

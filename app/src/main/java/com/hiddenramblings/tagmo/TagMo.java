55aa0cfb63d5de93cec59c49c822de6238d7738f
Abandoned Cart <twistedumbrella@gmail.com>
2021-11-18 08:42:16 -0500

    Update resources and add new menu icons
    Relocate wiki to a dedicated guide website

--- app/src/main/java/com/hiddenramblings/tagmo/TagMo.java
+++ app/src/main/java/com/hiddenramblings/tagmo/TagMo.java
@@ -53,14 +53,14 @@ public class TagMo extends Application {
 
     public static class Website {
         public static final String AMIIBOAPI = "https://www.amiiboapi.com/api/amiibo/";
-        public static final String API_LASTUPDATED = "https://www.amiiboapi.com/api/lastupdated/";
-        public static final String AMIIBOAPI_IMAGE = "https://raw.githubusercontent.com/N3evin/AmiiboAPI/master/images/icon_%08x-%08x.png";
+        public static final String LASTUPDATED = "https://www.amiiboapi.com/api/lastupdated/";
+        public static final String AMIIBO_IMAGE = "https://raw.githubusercontent.com/N3evin/AmiiboAPI/master/images/icon_%08x-%08x.png";
         public static final String TAGMO_GIT_API = "https://api.github.com/repos/HiddenRamblings/TagMo/releases/tags/";
-        public static final String TAGMO_WIKI = "https://github.com/HiddenRamblings/TagMo/wiki/Instructions";
+        public static final String TAGMO_WIKI = "https://tagmo.gitlab.io/";
         public static final String TAGMO_REDDIT = "https://www.reddit.com/r/tagmo/";
         public static final String WUMIIBO_APP = "https://appassets.androidplatform.net/assets/wumiibo/index.html";
         public static final String WUMIIBO_URI = "file:///android_asset/wumiibo/index.html";
-        public static final String WUMIIBO_WEB = "https://8bitdream.github.io/amiibo-generator/";
+        public static final String WUMIIBO_WEB = "https://tagmo.gitlab.io/wumiibo/";
     }
 
     @Pref

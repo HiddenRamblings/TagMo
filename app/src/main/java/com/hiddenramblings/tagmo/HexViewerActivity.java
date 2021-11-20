0cfae7762dc55fa50f9612a17dd9b055b7867389
Abandoned Cart <twistedumbrella@gmail.com>
2021-11-20 07:42:13 -0500

    Attempt to recycle data after failed decryption
    Validate data used for inline repair operations

--- app/src/main/java/com/hiddenramblings/tagmo/HexViewerActivity.java
+++ app/src/main/java/com/hiddenramblings/tagmo/HexViewerActivity.java
@@ -12,6 +12,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.eightbit.io.Debug;
 import com.hiddenramblings.tagmo.adapter.HexCodeAdapter;
 import com.hiddenramblings.tagmo.amiibo.KeyManager;
+import com.hiddenramblings.tagmo.nfctech.TagReader;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
@@ -39,8 +40,12 @@ public class HexViewerActivity extends AppCompatActivity {
             listView.setLayoutManager(new LinearLayoutManager(this));
             listView.setAdapter(adapter);
         } catch (Exception e) {
-            Debug.Log(e);
-            showErrorDialog(R.string.fail_display);
+            try {
+                adapter = new HexCodeAdapter(TagReader.getValidatedData(keyManager, tagData));
+            } catch (Exception ex) {
+                Debug.Log(e);
+                showErrorDialog(R.string.fail_display);
+            }
         }
     }
 

1f7f2cf96ee0d6a98e08eb3a143484bad2e00a63
Abandoned Cart <twistedumbrella@gmail.com>
2022-01-29 12:35:38 -0500

    Attempt to fix the broken replacement launch

--- app/src/main/java/com/hiddenramblings/tagmo/UpdateReceiver.java
+++ app/src/main/java/com/hiddenramblings/tagmo/UpdateReceiver.java
@@ -8,9 +8,10 @@ public class UpdateReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (Intent.ACTION_MY_PACKAGE_REPLACED.equals(intent.getAction()))
+        if (Intent.ACTION_MY_PACKAGE_REPLACED.equals(intent.getAction())) {
             context.startActivity(context.getPackageManager()
                     .getLaunchIntentForPackage(BuildConfig.APPLICATION_ID)
                     .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+        }
     }
 }

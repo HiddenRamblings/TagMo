2090ee4372679f289fe2337c54c882f7f51b2b5c
Abandoned Cart <twistedumbrella@gmail.com>
2022-04-05 19:19:21 -0400

    Move Flask BLE activity to conditional fragment

--- app/src/main/java/com/hiddenramblings/tagmo/NavPagerAdapter.java
+++ app/src/main/java/com/hiddenramblings/tagmo/NavPagerAdapter.java
@@ -1,5 +1,8 @@
 package com.hiddenramblings.tagmo;
 
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
@@ -7,6 +10,8 @@ import androidx.viewpager2.adapter.FragmentStateAdapter;
 class NavPagerAdapter extends FragmentStateAdapter {
     private BrowserFragment browserFragment = new BrowserFragment();
     private FoomiiboFragment foomiiboFragment = new FoomiiboFragment();
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
+    private FlaskFragment flaskFragment = new FlaskFragment();
 
     public NavPagerAdapter(FragmentActivity fa) {
         super(fa);
@@ -16,12 +21,15 @@ class NavPagerAdapter extends FragmentStateAdapter {
     public Fragment createFragment(int position) {
         if (position == 1)
             return foomiiboFragment;
+        else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2 && position == 2)
+            return flaskFragment;
         else
             return browserFragment;
     }
 
     @Override
     public int getItemCount() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) return 3;
         return 2;
     }
 
@@ -31,6 +39,11 @@ class NavPagerAdapter extends FragmentStateAdapter {
 
     public FoomiiboFragment getFoomiibo() {
         return foomiiboFragment;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
+    public FlaskFragment getFlask() {
+        return flaskFragment;
     }
 }
 

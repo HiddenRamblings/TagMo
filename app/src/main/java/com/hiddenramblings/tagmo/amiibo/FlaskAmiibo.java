8dae5efff8cec08088362ba9f0d71126889d0d6a
Abandoned Cart <twistedumbrella@gmail.com>
2022-06-01 05:33:18 -0400

    Support setting active Flask slot from the UI

---
+++ app/src/main/java/com/hiddenramblings/tagmo/amiibo/FlaskAmiibo.java
@@ -0,0 +1,23 @@
+package com.hiddenramblings.tagmo.amiibo;
+
+public class FlaskAmiibo {
+
+    private Amiibo amiibo;
+    private String flaskID;
+
+    public Amiibo getAmiibo() {
+        return amiibo;
+    }
+
+    public void setAmiibo(Amiibo amiibo) {
+        this.amiibo = amiibo;
+    }
+
+    public String getFlaskID() {
+        return flaskID;
+    }
+
+    public void setFlaskID(String flaskID) {
+        this.flaskID = flaskID;
+    }
+}

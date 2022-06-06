ff53014ffcf57485a3e6f6e05f5510c73ea58da2
Matthew English-Gallantry <matthew.gallantry@gmail.com>
2022-06-06 23:12:53 +0100

    Remove

--- app/src/main/java/com/hiddenramblings/tagmo/BluetoothLeService.java
+++ app/src/main/java/com/hiddenramblings/tagmo/BluetoothLeService.java
@@ -90,8 +90,6 @@ public class BluetoothLeService extends Service {
         try {
             new JSONObject(test);
         } catch (JSONException ex) {
-            // edited, to include @Arthur's comment
-            // e.g. in case JSONArray is valid as well...
             try {
                 new JSONArray(test);
             } catch (JSONException ex1) {

9010eab0d8f2cdff549c16ad36122c7248fd71f3
Abandoned Cart <twistedumbrella@gmail.com>
2022-06-26 07:29:39 -0400

    N2 Elite transition to full ViewPager2 support

--- app/src/main/java/com/hiddenramblings/tagmo/hardware/FlaskSlotFragment.java
+++ app/src/main/java/com/hiddenramblings/tagmo/hardware/FlaskSlotFragment.java
@@ -366,7 +366,7 @@ public class FlaskSlotFragment extends Fragment implements
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_bluup_flask, container, false);
+        return inflater.inflate(R.layout.fragment_flask_slot, container, false);
     }
 
     @Override

13f7b53755d7e682fba4e96eeeda95d743cd79d4
Abandoned Cart <twistedumbrella@gmail.com>
2022-06-26 06:11:00 -0400

    Begin transition to full ViewPager2 support

--- app/src/main/java/com/hiddenramblings/tagmo/NavPagerAdapter.java
+++ app/src/main/java/com/hiddenramblings/tagmo/NavPagerAdapter.java
@@ -1,13 +1,22 @@
 package com.hiddenramblings.tagmo;
 
+import android.os.Build;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
+import com.hiddenramblings.tagmo.hardware.EliteBankFragment;
+import com.hiddenramblings.tagmo.hardware.FlaskSlotFragment;
+
 class NavPagerAdapter extends FragmentStateAdapter {
     private final BrowserFragment browserFragment = new BrowserFragment();
     private final FoomiiboFragment foomiiboFragment = new FoomiiboFragment();
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
+    private final FlaskSlotFragment flaskFragment = new FlaskSlotFragment();
+    private final EliteBankFragment eliteFragment = new EliteBankFragment();
 
     public NavPagerAdapter(FragmentActivity fa) {
         super(fa);
@@ -17,13 +26,18 @@ class NavPagerAdapter extends FragmentStateAdapter {
     public Fragment createFragment(int position) {
         if (position == 1)
             return foomiiboFragment;
+        else if (position == 2)
+            return Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2
+                    ? flaskFragment :eliteFragment;
+        else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2 && position == 3)
+            return eliteFragment;
         else
             return browserFragment;
     }
 
     @Override
     public int getItemCount() {
-        return 2;
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2 ? 4 : 3;
     }
 
     public BrowserFragment getBrowser() {
@@ -32,6 +46,15 @@ class NavPagerAdapter extends FragmentStateAdapter {
 
     public FoomiiboFragment getFoomiibo() {
         return foomiiboFragment;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
+    public FlaskSlotFragment getFlaskSlots() {
+        return flaskFragment;
+    }
+
+    public EliteBankFragment getEliteBanks() {
+        return eliteFragment;
     }
 }
 

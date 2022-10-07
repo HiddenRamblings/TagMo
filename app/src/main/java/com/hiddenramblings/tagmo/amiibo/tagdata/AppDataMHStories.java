385b3aae3a60b642ef389cab2c8370e2dfcec8f1
Abandoned Cart <twistedumbrella@gmail.com>
2022-10-07 08:52:04 -0400

    Begin filling app data information (libamiibo)
    Add a courtesy copyright for derivative work

---
+++ app/src/main/java/com/hiddenramblings/tagmo/amiibo/tagdata/AppDataMHStories.java
@@ -0,0 +1,16 @@
+/* ====================================================================
+ * libamiibo Copyright (C) 2018 Benjamin Krämer
+ * Data/Settings/AppData/Games/MonsterHunterStories.cs
+ * ====================================================================
+ */
+
+package com.hiddenramblings.tagmo.amiibo.tagdata;
+
+import java.io.IOException;
+
+public class AppDataMHStories extends AppData {
+
+    public AppDataMHStories(byte[] appData) throws IOException {
+        super(appData);
+    }
+}

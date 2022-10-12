38505f7bda1941fd4578129145c2696aaba7eb79
Abandoned Cart <twistedumbrella@gmail.com>
2022-10-11 21:30:25 -0400

    Support second variant of save data circulating

--- app/src/main/java/com/hiddenramblings/tagmo/amiibo/tagdata/AppDataSplatoon.java
+++ app/src/main/java/com/hiddenramblings/tagmo/amiibo/tagdata/AppDataSplatoon.java
@@ -21,7 +21,6 @@ public class AppDataSplatoon extends AppData {
     String gameDataHex =                        "01 01 00 00" +
             "00 00 00 00 42 C0 7B 0A 36 FA 8F C1 48";
     byte[] gameDataBytes = TagArray.hexToByteArray(gameDataHex.replace(" ", ""));
-
     String appDataHex =                            "F5 09 60" +
             "C3 8E C5 52 EE E2 9A CE 27 71 5C 65 40 5B C3 F1" +
             "45 BF C6 3E 72 68 A8 9E DB 41 F4 25 8D 3A 4B 75" +
@@ -35,20 +34,43 @@ public class AppDataSplatoon extends AppData {
             "9E F3 63 B2 BO AE DD A2 5D 09 CC 9B 9B D1 D5 3E" +
             "88 BO 6D C2 AF 9D 66 7E 20 24 77 3B 88 7C AF 7F" +
             "88 9C B6 3D";
-    byte[] appDataBytes = TagArray.hexToByteArray(appDataHex.replace(" ", ""));
+
+    String gameDataHex2 =                        "01 01 00 00" +
+            "00 00 00 00 99 24 8D 4B 4C 84 9D FO A4";
+    byte[] gameDataBytes2 = TagArray.hexToByteArray(gameDataHex.replace(" ", ""));
+    String appDataHex2 =                            "98 OC F5" +
+            "AE D1 42 9B 40 1D 20 42 7D 85 28 77 62 OD FO C8" +
+            "9E B1 B2 AO 8A D4 5A 42 39 95 6E F3 FA C4 36 FB" +
+            "EC CB 4E C5 DC 8F 85 6E 88 FC 3E 37 5F 52 39 70" +
+            "05 75 23 BC 34 5F BD 2F 9F 54 B6 AD 39 B8 7D 19" +
+            "6E 63 68 DO EF 49 00 57 34 A9 82 22 BD 69 8E 16" +
+            "A3 77 7C A6 99 5B 83 4B 15 97 C3 12 9B 35 F9 87" +
+            "72 D6 2B 29 C2 OD 8A 72 57 FD 2F 78 E5 A1 7A DE" +
+            "98 C5 F4 6A OF A2 F7 B1 8F 1B D9 EO 1B 1E DD BB" +
+            "AO F3 48 5F OC 64 BE EB 04 7D D2 F7 FF B1 9C 7F" +
+            "02 56 C7 3D B6 D4 F8 7A 90 A1 55 7E D4 4A E1 6B" +
+            "78 CO 3F 99 89 47 37 37 ED 6A E2 C8 9E C5 32 67" +
+            "AA 42 4E 6D";
+
 
     public boolean checkSaveData(byte[] tagData) {
-        byte[] gameData = new byte[gameDataBytes.length];
-        System.arraycopy(tagData, 0xDC, gameData, 0, gameDataBytes.length);
-        return Arrays.equals(gameDataBytes, gameData);
+        byte[] saveData = new byte[gameDataBytes.length];
+        System.arraycopy(tagData, 0xDC, saveData, 0, gameDataBytes.length);
+        return Arrays.equals(gameDataBytes, saveData)
+                || Arrays.equals(gameDataBytes2, saveData);
     }
 
-    public byte[] injectSaveData(byte[] tagData) {
-        System.arraycopy(gameDataBytes, 0, tagData, 0xDC, gameDataBytes.length);
+    public byte[] injectSaveData(boolean source, byte[] tagData) {
+        System.arraycopy(
+                source ? gameDataBytes : gameDataBytes2, 0, tagData, 0xDC, gameDataBytes.length
+        );
         return tagData;
     }
 
-    public void injectSaveData() {
+    public void injectSaveData(boolean source) {
+        byte[] appDataBytes = source
+                ? TagArray.hexToByteArray(appDataHex.replace(" ", ""))
+                : TagArray.hexToByteArray(appDataHex2.replace(" ", ""));
         for (int i = 0; i < appDataBytes.length; i++) {
             appData.put(GAME_DATA_OFFSET + i, appDataBytes[i]);
         }

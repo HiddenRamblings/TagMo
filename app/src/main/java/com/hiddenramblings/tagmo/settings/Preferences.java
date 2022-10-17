d7d62f1f92588c1a4d27e518de070091160448ac
Abandoned Cart <twistedumbrella@gmail.com>
2022-10-17 10:24:25 -0400

    Set pref values to prevent copy / paste errors

--- app/src/main/java/com/hiddenramblings/tagmo/settings/Preferences.java
+++ app/src/main/java/com/hiddenramblings/tagmo/settings/Preferences.java
@@ -45,213 +45,243 @@ public class Preferences {
         prefs.edit().remove(pref).apply();
     }
 
+    private final String query = "query";
     public String query() {
-        return getString("query", null);
+        return getString(query, null);
     }
     public void query(String value) {
-        putString("query", value);
+        putString(query, value);
     }
 
+    private final String sort = "sort";
     public int sort() {
-        return getInt("sort", 1/*SORT.NAME*/);
+        return getInt(sort, 1/*SORT.NAME*/);
     }
     public void sort(int value) {
-        putInt("sort", value);
+        putInt(sort, value);
     }
 
+    private final String filterGameTitles = "filterGameTitles";
     public String filterGameTitles() {
-        return getString("query", null);
+        return getString(filterGameTitles, null);
     }
     public void filterGameTitles(String value) {
-        putString("filterGameTitles", value);
+        putString(filterGameTitles, value);
     }
 
+    private final String filterGameSeries = "filterGameSeries";
     public String filterGameSeries() {
-        return getString("query", null);
+        return getString(filterGameSeries, null);
     }
     public void filterGameSeries(String value) {
-        putString("filterGameSeries", value);
+        putString(filterGameSeries, value);
     }
 
+    private final String filterCharacter = "filterCharacter";
     public String filterCharacter() {
-        return getString("filterCharacter", null);
+        return getString(filterCharacter, null);
     }
     public void filterCharacter(String value) {
-        putString("filterCharacter", value);
+        putString(filterCharacter, value);
     }
 
+    private final String filterAmiiboSeries = "filterAmiiboSeries";
     public String filterAmiiboSeries() {
-        return getString("filterAmiiboSeries", null);
+        return getString(filterAmiiboSeries, null);
     }
     public void filterAmiiboSeries(String value) {
-        putString("filterAmiiboSeries", value);
+        putString(filterAmiiboSeries, value);
     }
 
+    private final String filterAmiiboType = "filterAmiiboType";
     public String filterAmiiboType() {
-        return getString("filterAmiiboType", null);
+        return getString(filterAmiiboType, null);
     }
     public void filterAmiiboType(String value) {
-        putString("filterAmiiboType", value);
+        putString(filterAmiiboType, value);
     }
 
+    private final String browserAmiiboView = "browserAmiiboView";
     public int browserAmiiboView() {
-        return getInt("browserAmiiboView", 1/*VIEW.COMPACT*/);
+        return getInt(browserAmiiboView, 1/*VIEW.COMPACT*/);
     }
     public void browserAmiiboView(int value) {
-        putInt("browserAmiiboView", value);
+        putInt(browserAmiiboView, value);
     }
 
+    private final String enable_tag_type_validation = "enable_tag_type_validation";
     public boolean enable_tag_type_validation() {
-        return getBoolean("enable_tag_type_validation", true);
+        return getBoolean(enable_tag_type_validation, true);
     }
     public void enable_tag_type_validation(boolean value) {
-        putBoolean("enable_tag_type_validation", value);
+        putBoolean(enable_tag_type_validation, value);
     }
 
+    private final String enable_automatic_scan = "enable_automatic_scan";
     public boolean enable_automatic_scan() {
-        return getBoolean("enable_automatic_scan", true);
+        return getBoolean(enable_automatic_scan, true);
     }
     public void enable_automatic_scan(boolean value) {
-        putBoolean("enable_automatic_scan", value);
+        putBoolean(enable_automatic_scan, value);
     }
 
+    private final String disable_foomiibo_browser = "disable_foomiibo_browser";
     public boolean disable_foomiibo_browser() {
-        return getBoolean("disable_foomiibo_browser", false);
+        return getBoolean(disable_foomiibo_browser, false);
     }
     public void disable_foomiibo_browser(boolean value) {
-        putBoolean("disable_foomiibo_browser", value);
+        putBoolean(disable_foomiibo_browser, value);
     }
 
+    private final String image_network_settings = "image_network_settings";
     public String image_network_settings() {
-        return getString("image_network_settings", SettingsFragment.IMAGE_NETWORK_ALWAYS);
+        return getString(image_network_settings, SettingsFragment.IMAGE_NETWORK_ALWAYS);
     }
     public void image_network_settings(String value) {
-        putString("image_network_settings", value);
+        putString(image_network_settings, value);
     }
 
+    private final String database_source_setting = "database_source_setting";
     public int database_source_setting() {
-        return getInt("database_source_setting", 0);
+        return getInt(database_source_setting, 0);
     }
     public void database_source_setting(int value) {
-        putInt("database_source_setting", value);
+        putInt(database_source_setting, value);
     }
 
+    private final String enable_power_tag_support = "enable_power_tag_support";
     public boolean enable_power_tag_support() {
-        return getBoolean("enable_power_tag_support", false);
+        return getBoolean(enable_power_tag_support, false);
     }
     public void enable_power_tag_support(boolean value) {
-        putBoolean("enable_power_tag_support", value);
+        putBoolean(enable_power_tag_support, value);
     }
 
+    private final String enable_elite_support = "enable_elite_support";
     public boolean enable_elite_support() {
-        return getBoolean("enable_elite_support", false);
+        return getBoolean(enable_elite_support, false);
     }
     public void enable_elite_support(boolean value) {
-        putBoolean("enable_elite_support", value);
+        putBoolean(enable_elite_support, value);
     }
 
+    private final String settings_elite_signature = "settings_elite_signature";
     public String settings_elite_signature() {
-        return getString("settings_elite_signature", "");
+        return getString(settings_elite_signature, "");
     }
     public void settings_elite_signature(String value) {
-        putString("settings_elite_signature", value);
+        putString(settings_elite_signature, value);
     }
 
+    private final String enable_flask_support = "enable_flask_support";
     public boolean enable_flask_support() {
-        return getBoolean("enable_flask_support", false);
+        return getBoolean(enable_flask_support, false);
     }
     public void enable_flask_support(boolean value) {
-        putBoolean("enable_flask_support", value);
+        putBoolean(enable_flask_support, value);
     }
 
+    private final String settings_disable_debug = "settings_disable_debug";
     public boolean settings_disable_debug() {
-        return getBoolean("settings_disable_debug", false);
+        return getBoolean(settings_disable_debug, false);
     }
     public void settings_disable_debug(boolean value) {
-        putBoolean("settings_disable_debug", value);
+        putBoolean(settings_disable_debug, value);
     }
 
+    private final String browserRootFolder = "browserRootFolder";
     public String browserRootFolder() {
-        return getString("browserRootFolder", null);
+        return getString(browserRootFolder, null);
     }
     public void browserRootFolder(String value) {
-        putString("browserRootFolder", value);
+        putString(browserRootFolder, value);
     }
 
+    private final String browserRootDocument = "browserRootDocument";
     public String browserRootDocument() {
-        return getString("browserRootDocument", null);
+        return getString(browserRootDocument, null);
     }
     public void browserRootDocument(String value) {
-        putString("browserRootDocument", value);
+        putString(browserRootDocument, value);
     }
 
+    private final String foomiiboOffset = "foomiiboOffset";
     public int foomiiboOffset() {
-        return getInt("foomiiboOffset", -1);
+        return getInt(foomiiboOffset, -1);
     }
     public void foomiiboOffset(int value) {
-        putInt("foomiiboOffset", value);
+        putInt(foomiiboOffset, value);
     }
 
+    private final String eliteBankCount = "eliteBankCount";
     public int eliteBankCount() {
-        return getInt("eliteBankCount", 200);
+        return getInt(eliteBankCount, 200);
     }
     public void eliteBankCount(int value) {
-        putInt("eliteBankCount", value);
+        putInt(eliteBankCount, value);
     }
 
+    private final String eliteActiveBank = "eliteActiveBank";
     public int eliteActiveBank() {
-        return getInt("eliteActiveBank", 0);
+        return getInt(eliteActiveBank, 0);
     }
     public void eliteActiveBank(int value) {
-        putInt("eliteActiveBank", value);
+        putInt(eliteActiveBank, value);
     }
 
+    private final String flaskActiveSlot = "flaskActiveSlot";
     public int flaskActiveSlot() {
-        return getInt("flaskActiveSlot", 0);
+        return getInt(flaskActiveSlot, 0);
     }
     public void flaskActiveSlot(int value) {
-        putInt("flaskActiveSlot", value);
+        putInt(flaskActiveSlot, value);
     }
 
+    private final String recursiveFolders = "recursiveFolders";
     public boolean recursiveFolders() {
-        return getBoolean("recursiveFolders", true);
+        return getBoolean(recursiveFolders, true);
     }
     public void recursiveFolders(boolean value) {
-        putBoolean("recursiveFolders", value);
+        putBoolean(recursiveFolders, value);
     }
 
+    private final String preferEmulated = "preferEmulated";
     public boolean preferEmulated() {
-        return getBoolean("preferEmulated", false);
+        return getBoolean(preferEmulated, false);
     }
     public void preferEmulated(boolean value) {
-        putBoolean("preferEmulated", value);
+        putBoolean(preferEmulated, value);
     }
 
+    private final String applicationTheme = "applicationTheme";
     public int applicationTheme() {
-        return getInt("applicationTheme", 0);
+        return getInt(applicationTheme, 0);
     }
     public void applicationTheme(int value) {
-        putInt("applicationTheme", value);
+        putInt(applicationTheme, value);
     }
 
+    private final String downloadUrl = "downloadUrl";
     public String downloadUrl() {
-        return getString("downloadUrl", null);
+        return getString(downloadUrl, null);
     }
     public void downloadUrl(String value) {
-        putString("downloadUrl", value);
+        putString(downloadUrl, value);
     }
 
+    private final String lastUpdatedAPI = "lastUpdatedAPI";
     public String lastUpdatedAPI() {
-        return getString("lastUpdatedAPI", null);
+        return getString(lastUpdatedAPI, null);
     }
     public void lastUpdatedAPI(String value) {
-        putString("lastUpdatedAPI", value);
+        putString(lastUpdatedAPI, value);
     }
 
+    private final String lastUpdatedGit = "lastUpdatedGit";
     public long lastUpdatedGit() {
-        return getLong("lastUpdatedGit", 0);
+        return getLong(lastUpdatedGit, 0);
     }
     public void lastUpdatedGit(long value) {
-        putLong("lastUpdatedGit", value);
+        putLong(lastUpdatedGit, value);
     }
 }

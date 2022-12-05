122e86b28fb13b32ded3c6070b58ae7f9c4c91d9
Abandoned Cart <twistedumbrella@gmail.com>
2022-12-05 08:16:53 -0500

    Convert networking / update code to Kotlin

---
+++ app/src/main/java/com/hiddenramblings/tagmo/GlideTagModule.kt
@@ -0,0 +1,82 @@
+package com.hiddenramblings.tagmo
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import android.util.Log
+import com.bumptech.glide.GlideBuilder
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.cache.ExternalPreferredCacheDiskCacheFactory
+import com.bumptech.glide.module.AppGlideModule
+import com.bumptech.glide.request.RequestOptions
+import com.hiddenramblings.tagmo.browser.Preferences
+import com.hiddenramblings.tagmo.eightbit.io.Debug
+
+@GlideModule
+class GlideTagModule : AppGlideModule() {
+    private fun isConnectionWiFi(context: Context): Boolean {
+        var result = false
+        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager?
+        if (Debug.isNewer(Build.VERSION_CODES.M)) {
+            cm?.run {
+                cm.getNetworkCapabilities(cm.activeNetwork)?.run {
+                    result = when {
+                        hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                        hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> false
+                        hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                        else -> false
+                    }
+                }
+            }
+        } else {
+            @Suppress("DEPRECATION")
+            cm?.run {
+                cm.activeNetworkInfo?.run {
+                    result = type == ConnectivityManager.TYPE_WIFI
+                            || type == ConnectivityManager.TYPE_ETHERNET
+                }
+            }
+        }
+        return result
+    }
+
+
+    private fun onlyRetrieveFromCache(context: Context, requestOptions: RequestOptions): RequestOptions {
+        val imageNetworkSetting = Preferences(context.applicationContext).image_network()
+        return if (IMAGE_NETWORK_NEVER == imageNetworkSetting) {
+            requestOptions.onlyRetrieveFromCache(true)
+        } else if (IMAGE_NETWORK_WIFI == imageNetworkSetting) {
+            requestOptions.onlyRetrieveFromCache(!isConnectionWiFi(context))
+        } else {
+            requestOptions.onlyRetrieveFromCache(false)
+        }
+    }
+
+    override fun applyOptions(context: Context, builder: GlideBuilder) {
+        val diskCacheSizeBytes = 1024 * 1024 * 128 // 128 MB
+        // The current size of the API image repo is 117.1 MB
+        builder.setDiskCache(
+            ExternalPreferredCacheDiskCacheFactory(
+                context, diskCacheSizeBytes.toLong()
+            )
+        )
+        builder.setLogLevel(Log.ERROR)
+        val requestOptions = onlyRetrieveFromCache(
+            context,
+            RequestOptions().diskCacheStrategy(DiskCacheStrategy.RESOURCE)
+        )
+        builder.setDefaultRequestOptions(requestOptions)
+    }
+
+    override fun isManifestParsingEnabled(): Boolean {
+        return false
+    }
+
+    companion object {
+        const val IMAGE_NETWORK_NEVER = "NEVER"
+        const val IMAGE_NETWORK_WIFI = "WIFI_ONLY"
+        const val IMAGE_NETWORK_ALWAYS = "ALWAYS"
+    }
+}
---
+++ app/src/main/java/com/hiddenramblings/tagmo/UpdateReceiver.kt
@@ -0,0 +1,73 @@
+package com.hiddenramblings.tagmo
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageInstaller
+import android.os.Build
+import android.os.Parcelable
+import android.widget.Toast
+import com.hiddenramblings.tagmo.browser.BrowserActivity
+import com.hiddenramblings.tagmo.eightbit.io.Debug.isNewer
+import java.net.URISyntaxException
+
+class UpdateReceiver : BroadcastReceiver() {
+    private inline fun <reified T : Parcelable>
+            Intent.parcelable(key: String): T? = when {
+        isNewer(Build.VERSION_CODES.TIRAMISU) ->
+            getParcelableExtra(key, T::class.java)
+        else ->
+            @Suppress("DEPRECATION") getParcelableExtra(key) as? T
+    }
+
+    override fun onReceive(context: Context, intent: Intent) {
+        val action = intent.action
+        intent.setPackage(context.packageName)
+        intent.flags = 0
+        intent.data = null
+        if (Intent.ACTION_MY_PACKAGE_REPLACED == action) {
+            var mainIntent: Intent? = Intent(context, BrowserActivity::class.java)
+            try {
+                mainIntent = context.packageManager
+                    .getLaunchIntentForPackage(BuildConfig.APPLICATION_ID)
+            } catch (ignored: Exception) {
+            }
+            startLauncherActivity(context, mainIntent!!.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK))
+        } else if (!BuildConfig.GOOGLE_PLAY && isNewer(Build.VERSION_CODES.LOLLIPOP)) {
+            when (intent.getIntExtra(PackageInstaller.EXTRA_STATUS, -1)) {
+                PackageInstaller.STATUS_PENDING_USER_ACTION -> {
+                    val activityIntent = intent.parcelable<Intent>(Intent.EXTRA_INTENT)
+                    if (null != activityIntent) {
+                        try {
+                            val intentUri = activityIntent.toUri(0)
+                            if (isNewer(Build.VERSION_CODES.LOLLIPOP_MR1)) {
+                                startLauncherActivity(
+                                    context, Intent.parseUri(
+                                        intentUri, Intent.URI_ALLOW_UNSAFE
+                                    ).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                                )
+                            } else {
+                                startLauncherActivity(
+                                    context, Intent.parseUri(intentUri, 0)
+                                        .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                                )
+                            }
+                        } catch (ignored: URISyntaxException) {
+                        }
+                    }
+                }
+                PackageInstaller.STATUS_SUCCESS -> {}
+                else -> {
+                    val error = intent.getStringExtra(PackageInstaller.EXTRA_STATUS_MESSAGE)
+                    if (!error!!.contains("Session was abandoned")) Toast.makeText(
+                        context, error, Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+        }
+    }
+
+    private fun startLauncherActivity(context: Context, intent: Intent) {
+        context.startActivity(intent)
+    }
+}

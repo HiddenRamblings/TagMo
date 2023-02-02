c1e985fa28e724137e4a8329d81965a74eae7bff
Abandoned Cart <twistedumbrella@gmail.com>
2023-02-02 10:29:15 -0500

    Bulk license for source used across projects

--- app/src/main/java/com/hiddenramblings/tagmo/update/UpdateReceiver.kt
+++ app/src/main/java/com/hiddenramblings/tagmo/update/UpdateReceiver.kt
@@ -2,48 +2,7 @@
  * ====================================================================
  * Copyright (c) 2021-2023 AbandonedCart.  All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * For the purpose of this license, the phrase "TagMo labels" shall
- * be used to refer to the labels "8-bit Dream", "TwistedUmbrella",
- * "TagMo" and "AbandonedCart" and these labels should be considered
- * the equivalent of any usage of the aforementioned phrase.
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- *
- * 3. All materials mentioning features or use of this software and
- *    redistributions of any form whatsoever must display the following
- *    acknowledgment unless made available by tagged, public "commits":
- *    "This product includes software developed for TagMo by AbandonedCart"
- *
- * 4. The TagMo labels must not be used in any form to endorse or promote
- *    products derived from this software without prior written permission.
- *    For written permission, please contact enderinexiledc@gmail.com
- *
- * 5. Products derived from this software may not be called by the TagMo labels
- *    nor may these labels appear in their names or product information without
- *    prior written permission of AbandonedCart.
- *
- * THIS SOFTWARE IS PROVIDED BY AbandonedCart AND TagMo ``AS IS'' AND ANY
- * EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE OpenSSL PROJECT OR
- * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
- * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
+ * https://github.com/AbandonedCart/AbandonedCart/blob/main/LICENSE#L4
  * ====================================================================
  *
  * The license and distribution terms for any publicly available version or

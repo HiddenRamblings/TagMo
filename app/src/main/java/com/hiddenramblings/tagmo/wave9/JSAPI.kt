7652edf56fb6f2ac173efad9666ae6ba20304fc3
Abandoned Cart <twistedumbrella@gmail.com>
2023-02-20 09:31:54 -0500

    Refactor JSAPI to existing NFC adaptation

--- app/src/main/java/com/hiddenramblings/tagmo/wave9/JSAPI.kt
+++ app/src/main/java/com/hiddenramblings/tagmo/wave9/JSAPI.kt
@@ -1,10 +1,10 @@
 package com.hiddenramblings.tagmo.wave9
 
-import android.nfc.tech.MifareUltralight
 import android.util.Base64
 import android.webkit.JavascriptInterface
 import android.webkit.WebView
 import com.hiddenramblings.tagmo.eightbit.io.Debug
+import com.hiddenramblings.tagmo.nfctech.NTAG215
 import java.io.IOException
 
 @Suppress("UNUSED")
@@ -12,73 +12,51 @@ class JSAPI(val activity: DimensionActivity, val web: WebView) {
 
     @JavascriptInterface
     fun readTag(page: Byte): String? {
-        val mifare = MifareUltralight.get(this.activity.tag)
-        return try {
-            mifare?.connect()
-            val payload = mifare.readPages(page.toInt())
-            Debug.info(javaClass, String.format(
-                "Payload %02X%02X%02X%02X %02X%02X%02X%02X %02X%02X%02X%02X %02X%02X%02X%02X",
-                payload[0], payload[1], payload[2], payload[3],
-                payload[4], payload[5], payload[6], payload[7],
-                payload[8], payload[9], payload[10], payload[11],
-                payload[12], payload[13], payload[14], payload[15]
-            ))
-            val encodeToString = Base64.encodeToString(payload, 0, 16, 0)
-            if (mifare == null) return encodeToString
+        val mifare = NTAG215[activity.tag]
+        return mifare?.let {
             try {
-                mifare.close()
-                encodeToString
-            } catch (e: IOException) {
-                encodeToString
+                it.connect()
+                // val payload = mifare.readPages(page.toInt())
+                val message = byteArrayOf(0x30, (page.toInt() and 0xFF).toByte())
+                it.transceive(message)?.let { payload ->
+                    Debug.info(javaClass, String.format(
+                        "Payload %02X%02X%02X%02X %02X%02X%02X%02X %02X%02X%02X%02X %02X%02X%02X%02X",
+                        payload[0], payload[1], payload[2], payload[3],
+                        payload[4], payload[5], payload[6], payload[7],
+                        payload[8], payload[9], payload[10], payload[11],
+                        payload[12], payload[13], payload[14], payload[15]
+                    ))
+                    closeTagSilently(it)
+                    Base64.encodeToString(payload, 0, 16, 0)
+                }
+            } catch (e2: IOException) {
+                closeTagSilently(it)
+                null
+            } catch (vomit: Throwable) {
+                closeTagSilently(it)
+                throw vomit
             }
-        } catch (e2: IOException) {
-            if (mifare != null) {
-                try {
-                    mifare.close()
-                } catch (ignored: IOException) { }
-            }
-            null
-        } catch (vomit: Throwable) {
-            if (mifare != null) {
-                try {
-                    mifare.close()
-                } catch (ignored: IOException) { }
-            }
-            throw vomit
         }
     }
 
     @JavascriptInterface
     fun writeTag(page: Byte, payload: String?): Boolean {
         val data = Base64.decode(payload, 0)
-        val ultralight = MifareUltralight.get(this.activity.tag)
-        return try {
-            ultralight.connect()
-            Debug.info(javaClass, String.format(
-                "Writing %02X%02X%02X%02X", data[0], data[1], data[2], data[3]
-            ))
-            ultralight.writePage(page.toInt(), data)
+        val mifare = NTAG215[activity.tag]
+        return mifare?.let {
             try {
-                ultralight.close()
-                true
-            } catch (e: IOException) {
-                false
+                it.connect()
+                Debug.info(javaClass, String.format(
+                    "Writing %02X%02X%02X%02X", data[0], data[1], data[2], data[3]
+                ))
+                it.writePage(page.toInt(), data)
+                closeTagSilently(it)
+            } catch (e2: IOException) {
+                closeTagSilently(it)
+            } catch (vomit: Throwable) {
+                closeTagSilently(it)
             }
-        } catch (e2: IOException) {
-            try {
-                ultralight.close()
-                true
-            } catch (e3: IOException) {
-                false
-            }
-        } catch (vomit: Throwable) {
-            try {
-                ultralight.close()
-                true
-            } catch (e4: IOException) {
-                false
-            }
-        }
+        } ?: false
     }
 
     private fun callJavaScript(methodName: String, vararg params: Any) {
@@ -88,5 +66,14 @@ class JSAPI(val activity: DimensionActivity, val web: WebView) {
         params.forEach { stringBuilder.append("'${if (it is String) it else it.toString()}',") }
         stringBuilder.append("'')}catch(error){console.error('ANDROID APP ERROR',error);}")
         this.web.loadUrl(stringBuilder.toString())
+    }
+
+    private fun closeTagSilently(mifare: NTAG215?) : Boolean {
+        return try {
+            mifare?.close()
+            true
+        } catch (ex: Exception) {
+            false
+        }
     }
 }

b1158316c8ad2fe05aa669b15fb3f6bdbd5e0e18
Abandoned Cart <twistedumbrella@gmail.com>
2023-03-09 12:22:17 -0500

    BlurView context cannot actually be `null`

--- app/src/main/java/eightbitlab/com/blurview/BlurView.kt
+++ app/src/main/java/eightbitlab/com/blurview/BlurView.kt
@@ -20,18 +20,18 @@ class BlurView : FrameLayout {
     @ColorInt
     private var overlayColor = 0
 
-    constructor(context: Context?) : super(context!!) {
+    constructor(context: Context) : super(context) {
         init(null, 0)
     }
 
-    constructor(context: Context?, attrs: AttributeSet?) : super(
-        context!!, attrs
+    constructor(context: Context, attrs: AttributeSet?) : super(
+        context, attrs
     ) {
         init(attrs, 0)
     }
 
-    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(
-        context!!, attrs, defStyleAttr
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context, attrs, defStyleAttr
     ) {
         init(attrs, defStyleAttr)
     }

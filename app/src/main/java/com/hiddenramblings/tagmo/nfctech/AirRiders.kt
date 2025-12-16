8d199baeb29a7d45df69b3a25102e72d01023d94
Abandoned Cart <twistedumbrella@gmail.com>
2025-12-16 03:48:17 -0500

    Strip unnecessary line endings for Kotlin code

--- app/src/main/java/com/hiddenramblings/tagmo/nfctech/AirRiders.kt
+++ app/src/main/java/com/hiddenramblings/tagmo/nfctech/AirRiders.kt
@@ -61,42 +61,42 @@ class AirRiders {
             log.appendLine("SAK : $sakHex")
             log.appendLine()
 
-            unlock(nfca, log);
+            unlock(nfca, log)
 
-            Thread.sleep(100);
+            Thread.sleep(100)
 
-            requestSram(nfca, log);
-            pollSramReady(nfca, log);
+            requestSram(nfca, log)
+            pollSramReady(nfca, log)
 
             // must be read first, otherwise TRANSFER_DIR bit is wrong
-            val sram = readPagesRange(nfca, 0xf0, 0xff, log);
+            val sram = readPagesRange(nfca, 0xf0, 0xff, log)
 
-            var dump = byteArrayOf();
-            dump += readPagesRange(nfca, 0x0, 0x1f, log);
-            dump += readPagesRange(nfca, 0x20, 0x3f, log);
-            dump += readPagesRange(nfca, 0x40, 0x5f, log);
-            dump += readPagesRange(nfca, 0x60, 0x7f, log);
-            dump += readPagesRange(nfca, 0x80, 0x9f, log);
-            dump += readPagesRange(nfca, 0xa0, 0xbf, log);
-            dump += readPagesRange(nfca, 0xc0, 0xdf, log);
-            dump += readPagesRange(nfca, 0xe0, 0xe9, log);
-            dump += ByteArray(8); // skip ea, eb
-            dump += readPagesRange(nfca, 0xec, 0xed, log);
-            dump += ByteArray(8); // skip ee, ef
-            dump += sram;
+            var dump = byteArrayOf()
+            dump += readPagesRange(nfca, 0x0, 0x1f, log)
+            dump += readPagesRange(nfca, 0x20, 0x3f, log)
+            dump += readPagesRange(nfca, 0x40, 0x5f, log)
+            dump += readPagesRange(nfca, 0x60, 0x7f, log)
+            dump += readPagesRange(nfca, 0x80, 0x9f, log)
+            dump += readPagesRange(nfca, 0xa0, 0xbf, log)
+            dump += readPagesRange(nfca, 0xc0, 0xdf, log)
+            dump += readPagesRange(nfca, 0xe0, 0xe9, log)
+            dump += ByteArray(8) // skip ea, eb
+            dump += readPagesRange(nfca, 0xec, 0xed, log)
+            dump += ByteArray(8) // skip ee, ef
+            dump += sram
 
-            sectorSelect(nfca, 1, log);
-            unlock(nfca, log);
-            Thread.sleep(100);
+            sectorSelect(nfca, 1, log)
+            unlock(nfca, log)
+            Thread.sleep(100)
 
-            dump += readPagesRange(nfca, 0x0, 0x1f, log);
-            dump += readPagesRange(nfca, 0x20, 0x3f, log);
-            dump += readPagesRange(nfca, 0x40, 0x5f, log);
-            dump += readPagesRange(nfca, 0x60, 0x7f, log);
-            dump += readPagesRange(nfca, 0x80, 0x9f, log);
-            dump += readPagesRange(nfca, 0xa0, 0xbf, log);
-            dump += readPagesRange(nfca, 0xc0, 0xdf, log);
-            dump += readPagesRange(nfca, 0xe0, 0xff, log);
+            dump += readPagesRange(nfca, 0x0, 0x1f, log)
+            dump += readPagesRange(nfca, 0x20, 0x3f, log)
+            dump += readPagesRange(nfca, 0x40, 0x5f, log)
+            dump += readPagesRange(nfca, 0x60, 0x7f, log)
+            dump += readPagesRange(nfca, 0x80, 0x9f, log)
+            dump += readPagesRange(nfca, 0xa0, 0xbf, log)
+            dump += readPagesRange(nfca, 0xc0, 0xdf, log)
+            dump += readPagesRange(nfca, 0xe0, 0xff, log)
 
 //            runOnUiThread {
 //                uiState = UiState(
@@ -117,58 +117,58 @@ class AirRiders {
     }
 
     private fun requestSram(nfca: NfcA, log: StringBuilder) {
-        val buffer = ByteArray(64);
-        buffer[0] = 1;
+        val buffer = ByteArray(64)
+        buffer[0] = 1
 
-        val crc = crc16Mcrf4xx(0xffff, buffer.slice(0..61).toByteArray(), 0);
-        buffer[62] = crc.shr(8).toByte();
-        buffer[63] = crc.and(0xff).toByte();
+        val crc = crc16Mcrf4xx(0xffff, buffer.slice(0..61).toByteArray(), 0)
+        buffer[62] = crc.shr(8).toByte()
+        buffer[63] = crc.and(0xff).toByte()
 
-        fastWriteSram(nfca, buffer, log);
+        fastWriteSram(nfca, buffer, log)
     }
 
     private fun sectorSelect(nfca: NfcA, sector: Int, log: StringBuilder) {
-        log.appendLine("Sending sector select pt1");
-        val sectorSelectCmd = byteArrayOf(0xC2.toByte(), 0xFF.toByte());
-        val res1 = nfca.transceive(sectorSelectCmd);
-        log.appendLine("Sector select pt1: ${res1.toHexSpaced()}");
+        log.appendLine("Sending sector select pt1")
+        val sectorSelectCmd = byteArrayOf(0xC2.toByte(), 0xFF.toByte())
+        val res1 = nfca.transceive(sectorSelectCmd)
+        log.appendLine("Sector select pt1: ${res1.toHexSpaced()}")
 
-        val sectorData = byteArrayOf(sector.toByte(), 0, 0, 0);
+        val sectorData = byteArrayOf(sector.toByte(), 0, 0, 0)
         try {
-            nfca.timeout = 10;
-            val res2 = nfca.transceive(sectorData);
-            log.appendLine("Sector select pt2: ${res2.toHexSpaced()}");
+            nfca.timeout = 10
+            val res2 = nfca.transceive(sectorData)
+            log.appendLine("Sector select pt2: ${res2.toHexSpaced()}")
         } catch (e: TagLostException) {
-            log.appendLine("Sector select pt2: tag lost (expected)");
-            nfca.timeout = 800;
+            log.appendLine("Sector select pt2: tag lost (expected)")
+            nfca.timeout = 800
         }
     }
 
     private fun pollSramReady(nfca: NfcA, log: StringBuilder) {
         while (true) {
-            log.appendLine("Polling for SRAM ready...");
-            val regs = readPagesRange(nfca, 0xed, 0xed, log);
-            if (regs[2].and(0b1000) != 0.toByte()) return;
+            log.appendLine("Polling for SRAM ready...")
+            val regs = readPagesRange(nfca, 0xed, 0xed, log)
+            if (regs[2].and(0b1000) != 0.toByte()) return
         }
     }
 
     private fun unlock(nfca: NfcA, log: StringBuilder) {
-        val id = nfca.tag.id;
+        val id = nfca.tag.id
 
         val authCmd = byteArrayOf(0x1B.toByte(),
             id[1].xor(id[3]).xor(0xAA.toByte()),
             id[2].xor(id[4]).xor(0x55.toByte()),
             id[3].xor(id[5]).xor(0xAA.toByte()),
             id[4].xor(id[6]).xor(0x55.toByte()),
-        );
+        )
         val resp = nfca.transceive(authCmd)
-        log.appendLine("PWD_AUTH: ${resp.toHexSpaced()}");
+        log.appendLine("PWD_AUTH: ${resp.toHexSpaced()}")
     }
 
     private fun fastWriteSram(nfca: NfcA, buffer: ByteArray, log: StringBuilder) {
-        require(buffer.size == 64);
+        require(buffer.size == 64)
 
-        val fastWriteCmd = byteArrayOf(0xA6.toByte(), 0xf0.toByte(), 0xff.toByte()) + buffer;
+        val fastWriteCmd = byteArrayOf(0xA6.toByte(), 0xf0.toByte(), 0xff.toByte()) + buffer
         log.appendLine("FAST_WRITE TX: ${fastWriteCmd.toHexSpaced()}")
 
         val resp = nfca.transceive(fastWriteCmd)
